baa5de44eacd83a50bf39233d4098a9c786ed351
Myriam <myriam.rihani00@gmail.com>
2021-04-27 11:19:14 +0200

    add motor ma

---
+++ moto_managmt.h
@@ -0,0 +1,15 @@
+/*
+ * moto_managmt.h
+ *
+ *  Created on: 27 Apr 2021
+ *      Author: myriamrihani
+ */
+
+#ifndef MOTO_MANAGMT_H_
+#define MOTO_MANAGMT_H_
+
+
+
+
+
+#endif /* MOTO_MANAGMT_H_ */
---
+++ motor_managmt.c
@@ -0,0 +1,10 @@
+/*
+ * motor_managmt.c
+ *
+ *  Created on: 27 Apr 2021
+ *      Author: myriamrihani
+ */
+
+
+
+
